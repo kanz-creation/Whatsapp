d73716eaf77f167468e356435be13794bbfe7a3f
kanz-creation <kaneez24@my.yorku.ca>
2021-09-05 00:10:14 -0400

    Chatbox header created

--- whatsapp/src/Chat.js
+++ whatsapp/src/Chat.js
@@ -1,10 +1,29 @@
 import React from 'react';
 import './Chat.css';
+import { Avatar, IconButton } from '@material-ui/core';
+import { MoreVert, SearchOutlined, AttachFile } from '@material-ui/icons';
 
 function Chat() {
   return (
     <div className="chat">
-      <h1>I am the chatbar</h1>
+      <div className="chat__header">
+        <Avatar alt="" src="" />
+        <div className="chat__headerInfo">
+          <h3>Thats grape </h3>
+          <p> Last seen 1 day ago </p>
+        </div>
+        <div className="chat__headerRight">
+          <IconButton>
+            <SearchOutlined />
+          </IconButton>
+          <IconButton>
+            <AttachFile />
+          </IconButton>
+          <IconButton>
+            <MoreVert />
+          </IconButton>
+        </div>
+      </div>
     </div>
   );
 }
